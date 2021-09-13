8351dd3bd9078d92fac8c644218c5385f1218661
Yuriy Pigovsky <yuriy.pigovsky@qvantel.com>
2021-09-13 09:57:16 +0300

    Add St.Iosafata church in Kyiv page

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,6 @@
 MENU = {
-    "Розклад Богослужінь": "https://docs.google.com/document/d/e/2PACX-1vRiXZbzC6yLChaScQPFxYBIZ4enH7qYNynOME6qwcVEN7d0NlUSfM7XjMzHEmCrjQ0bLkvnS4qFeWME/pub?embedded=true",
-    "Служителі": "https://docs.google.com/document/d/e/2PACX-1vTk4ViPH-QpKiqd8mkH-BnBozHNEWHUWuIEbhw_fbl8jAJQGhbUgLJNKO5-ldh1LcsS2y5oDz6137Hb/pub?embedded=true"
+    "Розклад Богослужінь": "https://docs.google.com/document/d/e/2PACX-1vTbYtR9xdjoIuCm3MQLJBGcRDxZUHOgr25Udw4va4Kq7e_7V4mJY6w5qq98Ejll1QCtkZZFZY9uH4zF/pub?embedded=true",
+    "Служителі": "https://docs.google.com/document/d/e/2PACX-1vR3jsdnd_Gf-TG97vQ5hYeJ4ebnR0pZHBjXFQhyCSxPUiqOPZJxazgsDDzYtrl4ubj8YaKhibev-5Wx/pub?embedded=true"
 }
 
 function init() {
