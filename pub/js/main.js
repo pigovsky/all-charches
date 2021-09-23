20ff9f1a39d9ca5b878a147f26a89b4878a89b44
Yuriy Pigovsky <yuriy.pigovsky@qvantel.com>
2021-09-23 10:16:00 +0300

    Add useful links and Where page

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,8 @@
 MENU = {
     "Розклад Богослужінь": "https://docs.google.com/document/d/e/2PACX-1vTbYtR9xdjoIuCm3MQLJBGcRDxZUHOgr25Udw4va4Kq7e_7V4mJY6w5qq98Ejll1QCtkZZFZY9uH4zF/pub?embedded=true",
-    "Служителі": "https://docs.google.com/document/d/e/2PACX-1vR3jsdnd_Gf-TG97vQ5hYeJ4ebnR0pZHBjXFQhyCSxPUiqOPZJxazgsDDzYtrl4ubj8YaKhibev-5Wx/pub?embedded=true"
+    "Служителі": "https://docs.google.com/document/d/e/2PACX-1vR3jsdnd_Gf-TG97vQ5hYeJ4ebnR0pZHBjXFQhyCSxPUiqOPZJxazgsDDzYtrl4ubj8YaKhibev-5Wx/pub?embedded=true",
+    "Корисне": "https://docs.google.com/document/d/e/2PACX-1vTtL9F7VIntnX4lcSUjliygWquhYGJx9MCJYuF2hBGaYrE1gpY9sF3wIY3fZBG7JFPdtcMnSBR9kfdr/pub?embedded=true",
+    "Де?": "https://psaltyr-u-wynogradnyku.web.app/map.html"
 }
 
 function init() {
