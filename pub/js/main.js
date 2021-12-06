e1581cce391d792d58e13af0a661ae568fb21f06
Jürij Pigowski <yuriy.pigovsky@qvantel.com>
2021-12-06 13:41:21 +0200

    Add Far-far away

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,7 @@
 MENU = {
     "Ініціативи": "https://docs.google.com/document/d/e/2PACX-1vTeYGHck4hDh8CwTlUvMxM3QGNoDDryzkbJ0GFRhEUGsV-qFI2BxPqHbGtmB20laMClyzqfohLPpgwN/pub?embedded=true",
-    "Громада": "pages/community.html"
+    "Громада": "pages/community.html",
+    "Далека далечінь": "https://docs.google.com/document/d/e/2PACX-1vTa8cpBTfGcJWSn_-t4EkLQL7B3C7CS-lnfFLZTcgTyrz5S-fcH5RwTuM3wNiAXBP-UUdvasyDH4-pe/pub?embedded=true"
 }
 
 function init() {
