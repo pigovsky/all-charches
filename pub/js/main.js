3d82f8182bceb981627b9661e7155c50af800629
Jürij Pigowski <yuriy.pigovsky@qvantel.com>
2021-12-26 08:40:18 +0200

    Add history; improve map link

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,7 @@
 MENU = {
     "Розклад Богослужінь": "https://docs.google.com/document/d/e/2PACX-1vRiXZbzC6yLChaScQPFxYBIZ4enH7qYNynOME6qwcVEN7d0NlUSfM7XjMzHEmCrjQ0bLkvnS4qFeWME/pub?embedded=true",
     "Служителі": "https://docs.google.com/document/d/e/2PACX-1vTk4ViPH-QpKiqd8mkH-BnBozHNEWHUWuIEbhw_fbl8jAJQGhbUgLJNKO5-ldh1LcsS2y5oDz6137Hb/pub?embedded=true"
+    "Історія": "https://docs.google.com/document/d/e/2PACX-1vSsuIO85IggikSeropYTfLOc6gHU9I5XZZC8AUNQelstsx7Y457Ne-EeVMCTUqsowMulSPnw_dn7Q13/pub?embedded=true"
 }
 
 function init() {
